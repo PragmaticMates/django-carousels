f4bc9b472838762b3d6c5e33019f065dc9d8cd85
eriktelepovsky <erik.telepovsky@gmail.com>
2021-05-29 15:26:18 +0200

    v0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-carousels',
-    version='0.1.0',
+    version='0.1.1',
     description='Django app for model-based carousels',
     long_description=open('README.md').read(),
     author='Pragmatic Mates',
