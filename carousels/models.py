ce5b8a1495e38403ca02fe17bd85abe70b7214db
eriktelepovsky <erik.telepovsky@gmail.com>
2023-11-12 19:03:45 +0100

    Django 4.2 support

--- carousels/models.py
+++ carousels/models.py
@@ -2,7 +2,7 @@ import json
 
 from django.db.models import JSONField
 from django.db import models
-from django.utils.translation import ugettext_lazy as _, pgettext_lazy
+from django.utils.translation import gettext_lazy as _, pgettext_lazy
 
 
 class Carousel(models.Model):
